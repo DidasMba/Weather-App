a988594c77c58b5e5727db571e91da34e2a6c76e
DidasMba <daniel.mbarushi@gmail.com>
2023-08-02 18:37:56 +0200

    fetch weather

---
+++ weatherapp.js
@@ -0,0 +1,6 @@
+// Function to fetch weather data for a given location
+function getWeatherData(location){
+      const apiKey = '90dbc5716a4345448e0161824230208';
+      const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}`;
+     
+}
