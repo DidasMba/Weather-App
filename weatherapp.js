8d93f80ec0d4773778e6bcdebe3b11256d4bca3d
DidasMba <daniel.mbarushi@gmail.com>
2023-08-03 15:48:37 +0200

    add json

--- weatherapp.js
+++ weatherapp.js
@@ -1,70 +1,31 @@
-  // Function to fetch weather data for a given location
-function getWeatherData(location) {
-    const apiKey = '90dbc5716a4345448e0161824230208'; // Replace this with your actual API key
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}`;
-  
-    fetch(apiUrl)
-      .then(response => response.json())
-      .then(data => {
-        // Process the weather data
-        const processedData = processWeatherData(data);
-  
-        // Display the weather information on the webpage
-        displayWeatherInfo(processedData);
-  
-        // Now you can use the processedData object in your Weather App
-        // For example, you can display the data on the UI or perform further operations with it
-      })
-      .catch(error => {
-        console.error('Error fetching weather data:', error);
-      });
-  }
-  
-  // Function to process the weather data and return relevant information
-  function processWeatherData(rawData) {
-    // Extract the required data from the raw JSON
-    const processedData = {
-      temperature: rawData.main.temp,
-      description: rawData.weather[0].description,
-      humidity: rawData.main.humidity,
-      windSpeed: rawData.wind.speed,
-      // Add more data fields as needed based on your app requirements
-    };
-  
-    return processedData;
-  }
-  
-  // Function to display weather information on the webpage
-  function displayWeatherInfo(weatherData) {
-    const weatherInfoElement = document.getElementById('weatherInfo');
-  
-    const content = `
-      <h2>Weather Information</h2>
-      <p><strong>Temperature:</strong> ${weatherData.temperature} °C</p>
-      <p><strong>Description:</strong> ${weatherData.description}</p>
-      <p><strong>Humidity:</strong> ${weatherData.humidity}%</p>
-      <p><strong>Wind Speed:</strong> ${weatherData.windSpeed} m/s</p>
-      <!-- Add more HTML elements as needed to display other weather information -->
-    `;
-  
-    weatherInfoElement.innerHTML = content;
-  }
-  
-  // Function to handle form submission
-  function handleFormSubmit(event) {
-    event.preventDefault();
-    const locationInput = document.getElementById('location');
-    const location = locationInput.value.trim();
-    if (location !== '') {
-      getWeatherData(location);
-      locationInput.value = ''; // Clear the input field after submission
+
+
+// WeatherAPI URL and your API key
+const apiKey = '90dbc5716a4345448e0161824230208';
+const apiUrl = 'https://api.weatherapi.com/v1/forecast.json';
+
+// Function to fetch weather data
+async function fetchWeatherData(location) {
+  try {
+    const response = await fetch(`${apiUrl}?key=${apiKey}&q=${location}&days=3`);
+    if (!response.ok) {
+      throw new Error('Weather data not available.');
     }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function extractWeatherData(jsonData) {
+    // Process the JSON data and return an object with required data
+    // For example:
+    const location = jsonData.location.name;
+    const temperature = jsonData.current.temp_c; // Temperature in Celsius
+    const weatherCondition = jsonData.current.condition.text;
+    
+    return { location, temperature, weatherCondition };
   }
   
-  // Add event listener to the form
-  const weatherForm = document.getElementById('weatherForm');
-  weatherForm.addEventListener('submit', handleFormSubmit);
-  
-
-
 
